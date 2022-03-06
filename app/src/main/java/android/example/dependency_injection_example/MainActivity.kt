c66ed27776b28210fc45760815ab540bff0cfe42
wasim15185 <wasimakram15185@gmail.com>
2022-03-06 12:59:46 +0530

    first commit

---
+++ app/src/main/java/android/example/dependency_injection_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.example.dependency_injection_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
